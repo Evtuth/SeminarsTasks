89a99edcad108a22227107f11a4b814cbf15e9c9
Vasiliy <evtuth@gmail.com>
2022-12-20 10:37:50 +0300

    added task9 square2

---
+++ task9_square2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int min = 0;
+int max = 0;
+
+if( a > b)
+{
+    min = b;
+    max = a;
+}
+else
+{
+    min = a;
+    max = b;
+}
+
+int square = min*min;
+
+if(square == max)
+{
+    Console.WriteLine("Число " + max + " является квадратом числа " + min);
+}
+else
+{
+   Console.WriteLine("Число " + max + " не является квадратом числа " + min); 
+}
