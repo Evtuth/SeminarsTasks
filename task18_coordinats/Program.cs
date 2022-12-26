ea6620752a50d8679e685d5f0148dffcd6421c7b
Vasiliy <evtuth@gmail.com>
2022-12-26 09:30:45 +0300

    added task18 chetverti

---
+++ task18_coordinats/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите значение четверти: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb == 1)
+{
+    Console.WriteLine("X > 0, Y > 0");
+}
+else if (numb == 2)
+{
+    Console.WriteLine("X < 0, Y > 0");
+}
+else if (numb == 3)
+{
+    Console.WriteLine("X < 0, Y < 0");
+}
+else if (numb == 4)
+{
+    Console.WriteLine("X > 0, Y < 0");
+}
+else if (numb <= 0 || numb > 3)
+{
+    Console.WriteLine("Неверное число!");
+}
