06500896c4d00fc0bf3fe5d2c7273ba0ce6ffa5f
Vasiliy <evtuth@gmail.com>
2023-01-14 08:17:14 +0300

    added task24 sum all numbers

---
+++ task24_Sum_numbers_all/Program.cs
@@ -0,0 +1,15 @@
+﻿int Sum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+string B = "123";
+int sum = Sum(A);
+Console.WriteLine(sum);
