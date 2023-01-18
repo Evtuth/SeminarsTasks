6e002078d9f0cdb933f3b2b362173003805053db
Vasiliy <evtuth@gmail.com>
2023-01-18 10:23:58 +0300

    added task33 have array this number

---
+++ task33_array_yes_or_not/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9,10);
+    }
+    return array;
+}
+
+int[] array = new int[12];
+array = FillArray(array);
+Console.Write("Ваш массив: ");
+Console.WriteLine(string.Join(", ", array));
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == a)
+        {
+            Console.WriteLine($"Число {a} присутствует в массиве!!!");
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Число {a} не присутствует в массиве!!!");
+            break;
+        }
+    }
+    return;
