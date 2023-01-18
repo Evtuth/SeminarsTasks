367283f2aa3ce048af6dbf459971be2e99c59ab3
Vasiliy <evtuth@gmail.com>
2023-01-18 09:47:15 +0300

    added task31 sum negative and positive numbers

---
+++ task31_sum_array/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+int[] FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9,10);
+    }
+    return array;
+}
+
+int[] array = new int[12];
+array = FillArray(array);
+
+Console.Write("Ваш случайный массив: ");
+Console.WriteLine(string.Join(", ", array));
+
+int SumPositive = 0;
+int SumNegative = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        SumPositive+=array[i];
+    }
+    else
+    {
+        SumNegative+=array[i];
+    }
+}
+
+Console.WriteLine($"Сумма положительных чисел: {SumPositive}, а сумма отрицательных чисел: {SumNegative}");
