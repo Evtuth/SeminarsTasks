a5f312312d7147656efe0d74abbc8f9175f18b9d
Vasiliy <evtuth@gmail.com>
2023-01-24 09:05:08 +0300

    added task46 2array matrix

---
+++ task46/Program.cs
@@ -0,0 +1,18 @@
+﻿void FillArrayWithRandom(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillArrayWithRandom(matrix, m, n);
