d986244ce09e038c65b02772140ff9e6e313101a
Vasiliy <evtuth@gmail.com>
2023-01-24 09:31:14 +0300

    added task51 sumdig 2array

---
+++ task51/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void FillArrayWithRandom(int[,] matrix, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumDigArray(int[,] matrix, int m, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(i==j)
+            {
+               sum += matrix[i, j];
+            }
+        }
+        Console.WriteLine();
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillArrayWithRandom(matrix, m, n);
+int sum = SumDigArray(matrix, m, n);
+Console.WriteLine($"Сумма чисел по главной диагонали: {sum}");
