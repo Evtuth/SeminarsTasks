e0a819e090a607da2f6672c4c4514b9ce940ba7c
Vasiliy <evtuth@gmail.com>
2023-02-02 09:07:56 +0300

    added recursia ot M do N

---
+++ task65/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача 65. Вывести все натуральные числа в промежутке от M до N.
+
+void PrintNumbers(int M, int N)
+{
+    if (N == M-1)
+    {
+        return;
+    }
+    PrintNumbers(M, N - 1);
+    Console.Write(N + " ");
+}
+
+Console.WriteLine("Введите первое число:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(M, N);
