e451af805bb396bbce0914b8da5212135fee6d8a
Vasiliy <evtuth@gmail.com>
2023-02-02 09:18:14 +0300

    added recursia sum numbers of number

---
+++ task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 67. Натйти сумму цифр в числе
+
+int Sum(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    return number%10 + Sum(number/10);
+}
+
+Console.WriteLine("Введите число:");
+int M = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(M);
+Console.WriteLine(sum);
