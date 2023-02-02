46af5cdb2dd8df6ed92fd71bd8af41b8229a3048
Vasiliy <evtuth@gmail.com>
2023-02-02 08:58:33 +0300

    added rekursia 1 to N

---
+++ task63/Program.cs
@@ -0,0 +1,15 @@
+﻿// задача 63. Вывести все натуральные числа в промежутке от 1 до N.
+
+void PrintNumbers(int number)
+{
+    if(number == 0)
+    {
+        return;
+    }
+    PrintNumbers(number - 1);
+    Console.Write(number + " ");
+}
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(N);
